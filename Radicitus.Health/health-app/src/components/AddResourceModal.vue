4daf66ebcd4e1c943c51615bafff9f1d8780494b
Nick Ganter <nickganter91@gmail.com>
2020-05-10 15:51:58 -0500

    starting link preview task

--- Radicitus.Health/health-app/src/components/AddResourceModal.vue
+++ Radicitus.Health/health-app/src/components/AddResourceModal.vue
@@ -6,9 +6,18 @@
 <script lang="ts">
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
+import ResourceItem from '../models/resource.model';
 
 @Component
 export default class AddResourceModal extends Vue {
+
+  public model = new ResourceItem();
+
+  public onUrlCompleted(url: string) {
+    this.$http.post(url).then(response => {
+      console.log(response);
+    });
+  }
 
 }
 </script>
